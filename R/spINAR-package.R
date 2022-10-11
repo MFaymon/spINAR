0f9b777054d232878be6cf3fdf1f725ad03ac3db
JonasRieger <jonas.rieger@tu-dortmund.de>
2022-10-11 22:46:51 +0200

    technical stuff

--- R/spINAR-package.R
+++ R/spINAR-package.R
@@ -1,6 +1,14 @@
-#' @title Title
+#' @title Estimation of semiparametric integer autoregressive models
 #'
-#' @description Bla
+#' @description A set of functions used for the following tasks.
+#' 1. Simulate INAR(p) data with arbitrary innovation distributions (parametric family or probability mass function).
+#' 2. Semiparametric estimation of INAR(p) model.
+#' 3. Semiparametric INAR Boostrap.
+#' 4. Fully parametric estimation of INAR(p) model.
+#' 5. Penalized (semiparametric) estimation of INAR(p) model.
+#'
+#' @references
+#' REFERENCES? arXiv paper?
 #'
 #' @import stats
 #' @import checkmate
--- R/spinar.R
+++ R/spinar.R
@@ -1,33 +1,40 @@
-#' spinar(p)
+#' @title Parameter estimation of (semi)parametric integer autoregressive models
 #'
-#' @param dat : dataset
-#' @param p : lag
+#' @description
+#' Performs bla bla.... EXPLAIN IN DETAIL!
 #'
-#' @return (alpha_1,..., alpha_p, pmf0, pmf1, ...) estimated parameters
+#' @param x [\code{integer}]\cr
+#' EXPLAIN IN DETAIL!.
+#' @param p [\code{integer(1)}]\cr
+#' Lag. EXPLAIN IN DETAIL!
+#'
+#' @return [\code{numeric(p+??)}]\cr
+#' Estimated parameters \code{(alpha_1,..., alpha_p, pmf0, pmf1, ...)}. EXPLAIN IN DETAIL!
+#'
+#' @examples
+#' # EXPLAIN IN DETAIL!
+#' spinar(c(2,3,1,1,1,1,1,1,3), 1)
+#' # 2nd EXAMPLE!
 #' @export
-#'
-#' @examples spinar(c(2,3,1,1,1,1,1,1,3), 1)
-#'2.362001e-11 4.205347e-09 6.128583e-01 5.021233e-10 3.871417e-01
-#'
 
-spinar <- function(dat, p) {
+spinar <- function(x, p) {
   # constraints for input
   checkmate::assert_integerish(p, lower = 1, min.len = 1, max.len = 1)
-  checkmate::assert_integerish(dat, min.len = p+1) # further requirements on input?
-  xmax <- max(dat)
-  if (max(dat) == min(dat)){
-    parameters <- c(c(1, 0)[0: 1L + (p==2)],
-                    c(rep(0, xmax),1))
+  checkmate::assert_integerish(x, min.len = p+1)
+  xmax <- max(x)
+  if (max(x) == min(x)){
+    #parameters <- c(c(1, 0)[0:1 + (p==2)], rep(0, xmax), 1) # is this correct??
+    parameters <- c(1, 0[p==2], rep(0, xmax), 1) # this is better to read
   }
   else {
   est <-
-    suppressWarnings(constrOptim(
-      theta = c(pmax(1e-5, acf(dat, plot = FALSE)$acf[seq_len(p)+1]), rep(1 / (xmax + 1), xmax)),
+    suppressWarnings(constrOptim( # catch warning and pass over?
+      theta = c(pmax(1e-5, acf(x, plot = FALSE)$acf[seq_len(p)+1]), rep(1 / (xmax + 1), xmax)),
       f = llspinar[[p]],
       grad = NULL,
       ui = .constrmat(p, xmax),
       ci = .constrvec(p, xmax),
-      dat = dat
+      dat = x
     ))
   parameters <- est$par
   parameters <- c(parameters[seq_len(p)],
