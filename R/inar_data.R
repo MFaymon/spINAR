c0d6a1f98d99ea8ee409e59db2a653125f49ce86
MFaymon <faymonville@statistik.tu-dortmund.de>
2022-10-18 13:38:19 +0200

    function to generate INAR data (semi)parametrically

---
+++ R/inar_data.R
@@ -0,0 +1,56 @@
+##################################################################################################################
+# Function to generate INAR(p) data for p \in {1,2}
+# We allow for general pmf's which can be generated parametrically or "manually" (semiparametric)
+##################################################################################################################
+
+## INAR(1) 
+
+sp_inar1 <- function(n, alpha, pmf) {
+  if(sum(pmf) != 1){warning("Sum of pmf entries has been standardized to 1.")}
+  pmf <- pmf/sum(pmf)
+  err <- sample(0:(length(pmf)-1), n, replace = TRUE, prob = pmf)
+  x <- numeric(n)
+  x[1] <- err[1]
+  for (i in 2:n) {
+    x[i] <- rbinom(1, x[i - 1], alpha) + err[i]
+  }
+  return(x)
+}
+
+# small examples
+
+n <- 1000
+alpha <- 0.5
+
+sp_inar1(n, alpha, dpois(0:20,1))
+sp_inar1(n, alpha, c(0.1,0.5,0.4))
+sp_inar1(n, alpha, c(0.1,0.5,0.2)) #warning
+
+## INAR(2)
+
+# small exmaples
+
+sp_inar2 <- function(n, alpha1, alpha2, pmf) {
+  if(sum(pmf) != 1){warning("Sum of pmf entries has been standardized to 1.")}
+  pmf <- pmf/sum(pmf)
+  err <- sample(0:(length(pmf)-1), n, replace = TRUE, prob = pmf)
+  x <- numeric(n)
+  x[1] <- err[1]
+  x[2] <- x[1] + err[2]
+  for (i in 3:n) {
+    x[i] <-
+      rbinom(1, x[i - 1], alpha1) + rbinom(1, x[i - 2], alpha2) + err[i]
+  }
+  return(x)
+}
+
+n <- 1000
+alpha1 <- 0.2
+alpha2 <- 0.3
+
+sp_inar2(n, alpha1, alpha2, dpois(0:20,1))
+sp_inar2(n, alpha1, alpha2, c(0.1,0.5,0.4))
+sp_inar2(n, alpha1, alpha2, c(0.1,0.5,0.2)) #warning
+
+
+
