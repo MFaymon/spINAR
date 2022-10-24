800d40cef5e769e0d33421a409e27d2d92686fc3
MFaymon <faymonville@statistik.tu-dortmund.de>
2022-10-24 11:11:59 +0200

    correct initialization for INAR(2)

--- R/spinar.R
+++ R/spinar.R
@@ -22,25 +22,31 @@ spinar <- function(x, p) {
   checkmate::assert_integerish(p, lower = 1, min.len = 1, max.len = 1, upper = 2)
   checkmate::assert_integerish(x, min.len = p+1)
   xmax <- max(x)
+  if(p==1){
+    theta <- c(max(acf(x, plot = FALSE)$acf[p+1], 1e-5), rep(1 / (xmax + 1), xmax))
+  }
+  if(p==2){
+    eacf1 <- acf(x, plot=FALSE)$acf[p+1]
+    eacf2 <- acf(x, plot=FALSE)$acf[p+2]
+    ealpha2 <- (eacf2-eacf1^2)/(1-eacf1^2)
+    ealpha1 <- (1-ealpha2)*eacf1
+    theta <- c(max(ealpha1, 1e-5), max(ealpha2, 1e-5), rep(1 / (xmax + 1), xmax))}
   if (max(x) == min(x)){
     parameters <- c(1, rep(0, p-1), 1, rep(0, xmax))
   }
   else {
-  est <-
-    suppressWarnings(constrOptim( # catch warning and pass over?
-      theta = c(pmax(1e-5, acf(x, plot = FALSE)$acf[seq_len(p)+1]), rep(1 / (xmax + 1), xmax)),
+    est <-suppressWarnings(constrOptim(
+      theta = theta,
       f = llspinar[[p]],
       grad = NULL,
       ui = .constrmat(p, xmax),
-      # substracting the corresponding smallest value
-      # to be in the feasible region ui %*% theta - ci >= 0
-      ci = .constrvec(p, xmax) + min(0, - 1e-5 + .constrmat(p, xmax) %*% c(pmax(1e-5, acf(x, plot = FALSE)$acf[seq_len(p)+1]), rep(1 / (xmax + 1), xmax)) - .constrvec(p, xmax)),
+      ci = .constrvec(p, xmax),
       dat = x
     ))
-  parameters <- est$par
-  parameters <- c(parameters[seq_len(p)],
-                  1-sum(parameters[-seq_len(p)]),
-                  parameters[-seq_len(p)])
+    parameters <- est$par
+    parameters <- c(parameters[seq_len(p)],
+                    1-sum(parameters[-seq_len(p)]),
+                    parameters[-seq_len(p)])
   }
   return(parameters)
 }
