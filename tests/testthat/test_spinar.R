37479cc8eed9e184220b1d9eab5c7431a2dd276b
Javiera Riffo <jariffo@uc.cl>
2022-10-26 08:58:44 +0200

    update test_spinar.R
    give a message of the error in the function expect_error()

--- tests/testthat/test_spinar.R
+++ tests/testthat/test_spinar.R
@@ -4,6 +4,9 @@ test_that("spinar output size", {
   expect_equal(length(spinar(c(2, 3, 1, 1), 1)), 5)
   expect_equal(length(spinar(c(2, 3, 1, 1, 0), 1)), 5)
   expect_equal(length(spinar(c(1, 1, 1, 0), 1)), 3)
+  expect_equal(spinar(c(2, 2, 2, 2, 2), 2), c(1, 0, 1, 0, 0))
+  expect_equal(spinar(c(2, 2, 2, 2, 2), 1), c(1, 1, 0, 0))
+  expect_equal(length(spinar(c(1, 3, 1, 1, 2, 2, 1, 2, 1, 4, 2, 2, 5, 5, 3, 2, 3, 3, 3, 3, 3, 2, 1, 2, 2, 4, 6, 5, 4, 8, 5, 8, 8, 10, 11), 2)), 14)
 })
 
 test_that("check sum pmf equal 1", {
@@ -21,14 +24,13 @@ test_that("alpha in range (0,1)", {
 })
 
 test_that("Non-interger p, non-vector dat or missing inputs should error", {
-  expect_error(spinar(c(1), 1)) # dat with p+1 entries
-  expect_error(spinar(c(1, 0), 2))
-  expect_error(spinar(c(2, 3, 1, 1, 0), "1")) # lag p must be integer
-  expect_error(spinar(c(2, 3, 1, 1, 0))) # argument "p" is missing
-  expect_error(spinar(c(2, 3), 2)) # no enough big input
-  expect_error(spinar(c(2, 2, 2, 2, 2), 2))
-  expect_error(spinar(c(2, 2, 2, 2, 2), 1))
+  expect_error(spinar(c(1), 1), "Assertion on 'x' failed: Must have length >= 2, but has length 1.")
+  expect_error(spinar(c(1, 0), 2), "Assertion on 'x' failed: Must have length >= 3, but has length 2.")
+  expect_error(spinar(c(2, 3, 1, 1, 0), "1"), "Assertion on 'p' failed: Must be of type 'integerish', not 'character'.")
+  expect_error(spinar(c(2, 3, 1, 1, 0)), 'argument "p" is missing, with no default')
+  expect_error(spinar(c(2, 3), 2), "Assertion on 'x' failed: Must have length >= 3, but has length 2.")
 })
+
 
 test_that("Check the type of output obtained", {
   expect_type(spinar(c(2, 0, 1), 1), "double")
