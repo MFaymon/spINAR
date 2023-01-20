881a2509757af10725e08fa597fe810944964d92
MFaymon <faymonville@statistik.tu-dortmund.de>
2023-01-20 16:04:04 +0100

    add package description and references

--- R/spINAR-package.R
+++ R/spINAR-package.R
@@ -1,14 +1,22 @@
-#' @title Estimation of semiparametric integer autoregressive models
+#' @title Simulate, estimate and bootstrap integer autoregressive (INAR) models
 #'
-#' @description A set of functions used for the following tasks.
-#' 1. Simulate INAR(p) data with given autoregressive coefficient(s) and given pmf.
-#' 2. Semiparametric estimation of INAR(p) model.
-#' 3. Parametric estimation of INAR(p) model.
-#' 4. Penalized semiparametric estimation of INAR(p) model with validation of the penalization parameters.
-#' 5. Semiparametric INAR Boostrap.
+#' @description Flexible simulation of INAR data using a general pmf to define
+#' the innovations' distribution, semiparametric and parametric estimation of
+#' INAR models (including a small sample refinement for the semiparametric
+#' setting) and different procedures to appropriately bootstrap INAR data.
 #'
 #' @references
-#' REFERENCES? arXiv paper? ...
+#' Faymonville, M., Jentsch, C., Weiß, C.H. und Aleksandrov, B. (2022).
+#' Semiparametric Estimation of INAR Models using Roughness Penalization.
+#' Statistical Methods & Applications.
+#'
+#' Jentsch, C. and Weiß, C. H. (2017), “Bootstrapping INAR Models”.
+#' Bernoulli 25(3), pp. 2359-2408.
+#'
+#' Drost, F., Van den Akker, R. and Werker, B. (2009), “Efficient estimation of
+#' auto-regression parameters and inovation distributions for semiparametric
+#' integer-valued AR(p) models”. Journal of the Royal Statistical Society.
+#' Series B 71(2), pp. 467-485.
 #'
 #' @import stats
 #' @import checkmate
