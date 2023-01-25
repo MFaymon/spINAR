a6a080ae34f29f3a88a32c647331e5f1060d1d3d
Javiera Riffo <jariffo@uc.cl>
2023-01-25 15:16:32 +0100

    Final version spinar_penal_val
    Increase speed by adding correct values for init1 and init2 and fixing the random values using set.seed()

--- tests/testthat/test_spinar_penal_validation.R
+++ tests/testthat/test_spinar_penal_validation.R
@@ -1,6 +1,8 @@
 # spinar_penal_val <- function(x, p, validation, penal1=NA, penal2=NA, over=NA, folds = 10, init1 = 1, init2 = 1)
 test_that("input", {
+  set.seed(123)
   tmp0 <- sample(1:10, 4, replace = TRUE)
+  set.seed(123)
   tmp1 <- spinar_sim(20, 1, 0.5, dpois(0:20,1))
   ######################## x ########################
   expect_error(spinar_penal_val(x = c(1), p = 1, validation = FALSE, penal1= 0, penal2= 0), "Assertion on 'x' failed: Must have length >= 2, but has length 1.")
@@ -26,7 +28,6 @@ test_that("input", {
   expect_error(spinar_penal_val(x = tmp1, p = 1, validation = TRUE, over = "l1"), "Assertion on 'over' failed")
   expect_error(spinar_penal_val(x = tmp1, p = 1, validation = TRUE, over = 4), "Assertion on 'over' failed")
   expect_error(spinar_penal_val(x = tmp1, p = 1, validation = TRUE, over = NULL), "Assertion on 'over' failed")
-  # Q: expect_error(spinar_penal_val(x = tmp1, p = 1, validation = TRUE, over = NA))
   ######################## folds ########################
   expect_error(spinar_penal_val(x = c(1,1,1), p = 2, validation = TRUE, over = "both", folds = 3), "Assertion on 'folds' failed")
   expect_error(spinar_penal_val(x = tmp1, p = 1, validation = TRUE, over = "both", penal1 =0, penal2=0, folds = "1"), "Assertion on 'folds' failed: Must be of type 'integerish', not 'character'.")
@@ -41,21 +42,22 @@ test_that("input", {
   expect_error(spinar_penal_val(x = tmp0, p = 1, validation = TRUE, over = "L1", penal1= 0.5, folds = 2), "if over = L1, no value for penal1 allowed")
   expect_warning(spinar_penal_val(x = tmp0, p = 1, validation = TRUE, over = "L1", init1 = 5,  init2 = 5, folds = 2), "value for penal2 is missing and is treated as zero")
   # validation = TRUE and over = 'L2'
-  # T :expect_warning(spinar_penal_val(x = tmp0, p = 1, validation = TRUE, over = "L2", folds = 2), 'value for penal1 is missing and is treated as zero')
-  # T: expect_warning(spinar_penal_val(x = tmp0, p = 1, validation = TRUE, over = "L1", folds=2), 'value for penal2 is missing and is treated as zero')
+  expect_warning(spinar_penal_val(x = tmp0, p = 1, validation = TRUE, over = "L2", folds = 2, init2=0.5), 'value for penal1 is missing and is treated as zero')
   # validation = TRUE and over = 'both'
-  # T: expect_warning(spinar_penal_val(x = tmp0, p = 1, validation = TRUE, over = "both", penal1 = 1, folds=2), "if over = both, input values for penal1 and penal2 are ignored")
+  expect_warning(spinar_penal_val(x = tmp0, p = 1, validation = TRUE, over = "both", penal1 = 1, folds=2, init1 =0 , init2 = 0.05), "if over = both, input values for penal1 and penal2 are ignored")
   # validation = FALSE
   expect_warning(spinar_penal_val(x = tmp0, p = 1, validation = FALSE, over = "L1", folds=2), 'values for penal1 or penal2 are missing, they are therefore treated as zero')
 })
 
 test_that("output size", {
   ######################## size ########################
+  set.seed(123)
   tmp2 <- sample(1:10, 6, replace = TRUE)
-  aux <- spinar_penal_val(x = tmp2, p = 2, validation = FALSE, penal1= 0, penal2= 0, folds=2)
-  expect_equal(length(aux), max(tmp2)+3)
+  aux1 <- spinar_penal_val(x = tmp2, p = 2, validation = FALSE, penal1= 0, penal2= 0, folds=2)
+  expect_equal(length(aux1), max(tmp2)+3)
+  aux2 <- spinar_penal_val(x = tmp2, p = 2, validation = TRUE, over = "L1", penal2= 0, init1=0, folds=2)
+  expect_true(length(aux2$penal1_opt)==1)
   ######################## type ########################
-  expect_type(aux, "double")
-  })
-
+  expect_type(aux1, "double")
+})
 
